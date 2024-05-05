b51afbb36033103e7d3c7406ad66267bf2a7e218
Alex Easey <alexeasey@hotmail.com>
2024-05-05 18:41:26 +1000

    Added CreateJobViewController

---
+++ IOSAssignment3/IOSAssignment3/CreateJobsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CreateJobsViewController.swift
+//  IOSAssignment3
+//
+//  Created by Alex Easey on 5/5/2024.
+//
+
+import UIKit
+
+class CreateJobsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
